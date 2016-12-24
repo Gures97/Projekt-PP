1acc01584dfc1776c5bed7740f1d1ef3f6a12f3f
gures97 <19gures97@gmail.com>
2016-12-24 16:44:39 +0100

    Deklaracja struktury listy

--- Projekt-PP/lista.c
+++ Projekt-PP/lista.c
@@ -1,1 +1,2 @@
+#include "lista.h"
 
--- Projekt-PP/lista.h
+++ Projekt-PP/lista.h
@@ -1,1 +1,11 @@
+#ifndef LISTA_H
+#define LISTA_H
 
+typedef struct element{
+    char word_ang[35];
+    char word_pol[3][35];
+    element * next;
+} ELEMENT;
+
+typedef ELEMEMNT *Lista;
+#endif // LISTA_H
