2805f25c1fc0b70e9883f359609cf03bce10177b
gures97 <19gures97@gmail.com>
2017-01-05 21:33:33 +0100

    Dodalem podstawowe UI

--- Projekt-PP/main.c
+++ Projekt-PP/main.c
@@ -2,14 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lista.h"
+#include "ui.h"
 
 int main()
 {
-    Lista lista;
-    lista = init_list(3);
+    char q = 0;
+    Lista slownik = NULL;
+    while(q != 'q'){
+        printf("=====================\n");
+        printf("       SLOWNIK\n");
+        printf("=====================\n\n\n");
+        printf("Liczba elementow slownika: %d\n\n", liczba_elementow(slownik));
 
-    printf("Hello world!\n");
-    printf("Ilosc elementow listy: %d", liczba_elementow(lista));
+        printf("1. Zobacz slownik.\n");
+        printf("2. Dodaj nowe slowo.\n");
+        printf("3. Zapisz slownik do pliku.\n");
+        printf("4. Wczytaj slownik z pliku.\n");
+        printf("q. Wyjdz z programu.\n");
 
+        komunikat(q);
+
+        printf("Wybieram opcje: ");
+        fflush(stdin);
+        scanf("%c",&q);
+        if(q != 'q')
+            system("cls");
+    }
     return 0;
 }
